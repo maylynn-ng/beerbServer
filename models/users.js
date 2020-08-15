f06e1c8ee3e2c2c91c29f7e7e7ed9b3058ceff01
Maylynn <maylynn.ng94@gmail.com>
2020-08-15 16:18:39 +0100

    feat: define user model

--- models/users.js
+++ models/users.js
@@ -1,22 +1,14 @@
 import sequelize from './index.js';
 
-const User = sequelize.define(
-  'User',
-  {
-    // Model attributes are defined here
-    firstName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    lastName: {
-      type: DataTypes.STRING,
-      // allowNull defaults to true
-    },
+const User = sequelize.define('User', {
+  Name: {
+    type: DataTypes.STRING,
+    allowNull: false,
   },
-  {
-    // Other model options go here
-  }
-);
+  Username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
 
-// `sequelize.define` also returns the model
 console.log(User === sequelize.models.User); // true
