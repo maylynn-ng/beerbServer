b6bd321f3f6309dec7a09bc380f034ce44956ad1
marijana <marijana.delbianco@gmail.com>
2020-08-15 16:30:43 +0200

    refactor(server): added gitignore, moved sequelize to index.js

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
+require('dotenv').config();
+
 const express = require('express');
 const router = require('./router/routes');
+const Sequelize = require('sequelize');
 const app = express();
 
 const port = process.env.PORT;
+
+const sequelize = new Sequelize(process.env.PG_URI);
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 app.listen(port, () => {
   console.log(`Serving you up on http://localhost:${port}`);
